ab36dadf8f6c0162f36f1206c237b2984291265e
David W <skatejoe1@gmail.com>
2014-05-23 20:17:32 +0200

    add basic drawing

--- src/controller.py
+++ src/controller.py
@@ -1,5 +1,6 @@
 import gui
 import random
+import numpy as np
 from PySide import QtCore, QtGui
 
 class Example(QtGui.QWidget):
@@ -9,17 +10,36 @@ class Example(QtGui.QWidget):
 		self.CELL_SIZE = 50
 		self.WALL_SIZE = 10
 		self.SOLUTION_SIZE = 25
-		self.translate = [self.WALL_SIZE/2,self.WALL_SIZE/2]
+		self.mouseDown = False
 	def paintEvent(self, event):
 		qp = QtGui.QPainter()
 		qp.begin(self)
 		self.drawPuzzle(event, qp)
 		qp.end()
-	def drawPuzzle(self, event, qp):
-		qp.setPen(QtCore.Qt.black)
+	def mousePressEvent(self,event):
+		self.mouseDown = True
+		self.click(np.asarray([event.pos().x(),event.pos().y()]))
+		self.update()
+	def click(self,clickpos):
+		translate = self.getTranslation()
+		clickpos-=translate
+		clickpos = (clickpos/self.CELL_SIZE)
+		self.puzzle.puzzlemap[0][1] = ' '
+		print(clickpos)
+	def mouseReleaseEvent(self,event):
+		self.mouseDown = False
+	def mouseMoveEvent(self,event):
+		pass
+		#if(self.mouseDown):
+		#	print(str(event.pos()))
+	def getTranslation(self):
 		size = self.size()
 		maxsize = [self.puzzle.width*self.CELL_SIZE,self.puzzle.height*self.CELL_SIZE]
-		#self.translate = [(size.width-maxsize[0])/2,(size.height-maxsize[1])/2]
+		translate = [(size.width()-maxsize[0])/2,(size.height()-maxsize[1])/2]
+		return translate
+	def drawPuzzle(self, event, qp):
+		qp.setPen(QtCore.Qt.black)
+		translate = self.getTranslation()
 		#draw grid
 		for i in range(self.puzzle.width+1):
 			for j in range(self.puzzle.height+1):
@@ -27,19 +47,19 @@ class Example(QtGui.QWidget):
 				y1 = j*self.CELL_SIZE
 				x2 = (self.puzzle.width)*self.CELL_SIZE
 				y2 = j*self.CELL_SIZE
-				x1+=self.translate[0]
-				y1+=self.translate[1]
-				x2+=self.translate[0]
-				y2+=self.translate[1]
+				x1+=translate[0]
+				y1+=translate[1]
+				x2+=translate[0]
+				y2+=translate[1]
 				qp.drawLine(QtCore.QPoint(x1,y1),QtCore.QPoint(x2,y2))
 				x1 = i*self.CELL_SIZE
 				y1 = 0
 				x2 = i*self.CELL_SIZE
 				y2 = (self.puzzle.height)*self.CELL_SIZE
-				x1+=self.translate[0]
-				y1+=self.translate[1]
-				x2+=self.translate[0]
-				y2+=self.translate[1]
+				x1+=translate[0]
+				y1+=translate[1]
+				x2+=translate[0]
+				y2+=translate[1]
 				qp.drawLine(QtCore.QPoint(x1,y1),QtCore.QPoint(x2,y2))
 		#draw walls
 		for j,line in enumerate(self.puzzle.puzzlemap):
@@ -49,15 +69,15 @@ class Example(QtGui.QWidget):
 					if(j%2==0 and i%2==1):
 						x = (i-1)*self.CELL_SIZE/2-self.WALL_SIZE/2
 						y = j*self.CELL_SIZE/2-self.WALL_SIZE/2
-						x+=self.translate[0]
-						y+=self.translate[1]
+						x+=translate[0]
+						y+=translate[1]
 						qp.fillRect(x,y,self.CELL_SIZE+self.WALL_SIZE,self.WALL_SIZE,QtCore.Qt.black)
 					#vertical
 					if(j%2==1):
 						x = i*self.CELL_SIZE/2-self.WALL_SIZE/2
 						y = (j-1)*self.CELL_SIZE/2-self.WALL_SIZE/2
-						x+=self.translate[0]
-						y+=self.translate[1]
+						x+=translate[0]
+						y+=translate[1]
 						qp.fillRect(x,y,self.WALL_SIZE,self.CELL_SIZE+self.WALL_SIZE,QtCore.Qt.black)
 		#draw solution
 		for j,line in enumerate(self.puzzle.puzzlemap):
@@ -66,15 +86,15 @@ class Example(QtGui.QWidget):
 					#horizontal
 					x = (i-1)*self.CELL_SIZE/2-self.SOLUTION_SIZE/2
 					y = j*self.CELL_SIZE/2-self.SOLUTION_SIZE/2
-					x+=self.translate[0]
-					y+=self.translate[1]
+					x+=translate[0]
+					y+=translate[1]
 					qp.fillRect(x,y,self.CELL_SIZE+self.SOLUTION_SIZE,self.SOLUTION_SIZE,QtCore.Qt.red)
 				if(el=='|'):
 					#vertical
 					x = i*self.CELL_SIZE/2-self.SOLUTION_SIZE/2
 					y = (j-1)*self.CELL_SIZE/2
-					x+=self.translate[0]
-					y+=self.translate[1]
+					x+=translate[0]
+					y+=translate[1]
 					qp.fillRect(x,y,self.SOLUTION_SIZE,self.CELL_SIZE,QtCore.Qt.red)
 
 class Model(object):
